6fde0256b74ac97d35269b046cb94520ded3f895
alphy_ren <alphy.ren@gmail.com>
2015-09-28 14:09:13 +0800

    fix bugs of build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-uglifyjs ../portal/public/main.js -o ../portal/public/main.min.js --source-map ../portal/public/main.js.map --source-map-url ../portal/public/main.js.map
+uglifyjs ../portal/public/main.js -m -o ../portal/public/main.min.js
+#uglifyjs ../portal/public/main.js -m -o ../portal/public/main.min.js --source-map ../portal/public/main.js.map --source-map-url ../portal/public/main.js.map
